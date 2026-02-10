e0f4329e2b5fb357dfe3ca975724d563ed601a8b
aniegboks <aniegbokavictor2024@gmail.com>
2026-02-10 12:29:05 +0100

    fix bugs

---
+++ next-sitemap.config.ts
@@ -0,0 +1,11 @@
+import type { IConfig } from 'next-sitemap';
+
+const config: IConfig = {
+  siteUrl: 'https://remm-technologies-partner.vercel.app',
+  generateRobotsTxt: true, // automatically creates robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
+
+export default config;
