a0dcb01dc62f17a6e4effae01fd8cd9cd9ca2db2
aniegboks <aniegbokavictor2024@gmail.com>
2026-02-10 12:38:49 +0100

    add site-map

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,8 +1,8 @@
 import type { IConfig } from 'next-sitemap';
 
 const config: IConfig = {
-  siteUrl: 'https://remm-technologies-partner.vercel.app',
-  generateRobotsTxt: true, // automatically creates robots.txt
+  siteUrl: 'https://remm-technologies-partner.vercel.app', // your canonical URL
+  generateRobotsTxt: true,
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 5000,
